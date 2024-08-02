f163c99b6438c5e2bfa3362d1f5ea0259ed8bd31
scythianeagle <132835586+scythianeagle@users.noreply.github.com>
2024-08-03 02:22:17 +0330

    Update HysteriaII.sh

--- HysteriaII.sh
+++ HysteriaII.sh
@@ -87,7 +87,7 @@ auth: $new_password
 transport:
   type: udp
 tls:
-  sni: bing.com
+  sni: ii.saka-eagle.icu
   insecure: true
 bandwidth:
   up: 100 mbps
@@ -262,7 +262,7 @@ auth: $password
 transport:
   type: udp
 tls:
-  sni: bing.com
+  sni: ii.saka-eagle.icu
   insecure: true
 bandwidth:
   up: 100 mbps
